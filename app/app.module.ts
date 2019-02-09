dafeb7217bef4c1809f5779c6bd84125a6ed2175
sumit270296 <sksksharma0@gmail.com>
2019-02-09 18:07:01 +0530

    Adding and linking hyperlinks

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { FormsModule }   from '@angular/forms';
 import { AppComponent } from './app.component';
 import { MovieService } from './movie/movie-service';
@@ -7,19 +8,32 @@ import { AddMovieComponent } from './movie/add-movie.component';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { HeaderComponent } from './includes/header.component';
 import { MainComponent } from './includes/main.component';
+import { LegislationComponent } from './pages/legislations.component';
+import { StepComponent } from './pages/step.component';
+import { AboutComponent } from './pages/about.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     AddMovieComponent,
     HeaderComponent,
-    MainComponent
+    MainComponent,
+    AboutComponent,
+    LegislationComponent,
+    StepComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      {path: 'about', component: AboutComponent },
+      { path: 'legislation', component: LegislationComponent },
+      { path: 'step', component: StepComponent }
+    ])
   ],
+  
   providers: [MovieService, HttpClient],
   bootstrap: [AppComponent]
 })
---
+++ app/pages/legislations.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'legislation-page',
+  templateUrl: './legislations.component.html',
+  styleUrls: ['../css/bootstrap.min.css', '../css/mdb.min.css']
+})
+export class LegislationComponent implements OnInit{
+    ngOnInit(){
+
+    }
+}
